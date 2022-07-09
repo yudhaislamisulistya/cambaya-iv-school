54ea4c17a65f2113acb8a89bc8324d4aa1235a6b
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-07-09 21:36:29 +0800

    first commit

---
+++ app/Controllers/BerandaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BerandaController extends BaseController
+{
+    public function index(){
+        return view('index');
+    }
+}
